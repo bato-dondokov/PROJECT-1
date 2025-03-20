4f4952b0880345e894ac8e8be4ac7fdba39f70f2
Bato <bato.dondokov.it@gmail.com>
2025-03-20 11:14:51 +0800

    changed show labels, rectangles with diff colors

--- yolo11n-obb-test.py
+++ yolo11n-obb-test.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO("results/YOLO11X-OBB7/weights/best.pt")
 
-model.predict("datasets/dataset/test/images/632.jpg", save_txt=True)
+model.predict("datasets/dataset/valid/images/649.jpg", save_txt=True)
